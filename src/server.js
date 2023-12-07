1622aa8de8cbb14d69500734c0e43fb6584b0d2f
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-12-07 15:16:56 +0530

    added cors extention

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import "./connection.js";
 import userRoute from "../Routes/userRoute.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
